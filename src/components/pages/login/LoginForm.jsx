9afc0b51ce187ca6ac991b2cbd9e0ee4d9ddf161
angelo mbunga <a.mbunga@gmail.com>
2024-01-02 20:11:40 +0100

    ♻️ F01: LOGIN: Create LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,29 +1,29 @@
+import React from 'react'
 import { useState } from 'react'
 
-export default function LoginPage() {
-  // ETAT
-  const [ inputValue, setInputValue] = useState('');
+export default function  () {
+    // ETAT
+    const [ inputValue, setInputValue] = useState('');
 
-  // COMPORTEMENT
-  const handleChange = (event) => {
+    // COMPORTEMENT
+    const handleChange = (event) => {
     setInputValue(event.target.value);
-  }
-  const handleSubmit = (event) => {
+    }
+
+    const handleSubmit = (event) => {
     event.preventDefault();
     alert("Bonjour " + event.target[0].value);
     setInputValue("");
-  }
+}
 
   // RENDU
   return (
-    <div className="card">
-    <h1>Bienvenue chez vous</h1>
-    <br />
-    <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
+    <form action="submit" onSubmit={handleSubmit}>
+        <h1>Bienvenue chez vous</h1>
+        <br />
+        <h2>Connectez-vous</h2>
         <input type="text" value={inputValue} name="inputValue" placeholder='Entrez votre prénom...' onChange={handleChange} required/>
         <button>Accéder à votre espace</button>
-      </form>
-    </div>
+    </form>
   )
 }
