65aec682e59f7b7153fce10dd5959125090cae50
angelo mbunga <a.mbunga@gmail.com>
2024-03-24 19:45:49 +0100

    🚧 F09 : PANEL > Add 'toogle menu display depending on menu state and user role'

--- src/components/pages/order/main/Menu.jsx
+++ src/components/pages/order/main/Menu.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import Card from '../../../reusable-ui/Card';
 import { theme } from '../../../../theme';
 import { fakeMenu1 } from '../../../../fakeData/fakeMenu';
 import { formatPrice, ajustPrice } from '../../../../utils/maths';
+import MenuEmpty from './MenuEmpty';
+import OrderContext from '../../../../context/OrderContext';
 
 export default function Menu() {
 
   const [menu, setMenu ] = useState(fakeMenu1);
+  const [isMenuEmpty, setIsMenuEmpty ] = useState(false);
+  const {isAdminMode,setAdminMode} = useContext(OrderContext)
   
   function handleDelete(id) {
 
@@ -16,20 +20,45 @@ export default function Menu() {
     const menuUpdated = menuCopy.filter(item => item.id !== id)
 
     setMenu(menuUpdated);
+    testMenu(menu)
   }
+
+  function testMenu(menu) {
+
+    const nbMenuItems = Object.keys(menu).length;
+
+    if (nbMenuItems <= 1) {
+      setIsMenuEmpty(true)
+    }
+
+  }
+  
   return (
     <MenuStyled>
-      {menu.map(({title, imageSource, id, price}) => {
-        return (
-          <Card
-            key={id}
-            title={title}
-            imageSource={imageSource}
-            leftDescription={formatPrice(price)}
-            onClick={() => handleDelete(id)}
-          />
-        )
-      })}
+      {isMenuEmpty
+        ? !isAdminMode 
+            ? <MenuEmpty 
+                text1="Le menu est vide ?" 
+                text2="cliquez ci-dessous pour le réinitialiser" 
+                btnLabel='Générer nouveaux produits' 
+                btnClassName='generate-items-btn'
+              /> 
+            : <MenuEmpty 
+                text1="Victime de notre success !" 
+                text2="De nouvelles recettes arrivent bientot"
+              />
+        : menu.map(({title, imageSource, id, price}) => {
+            return (
+              <Card
+                key={id}
+                title={title}
+                imageSource={imageSource}
+                leftDescription={formatPrice(price)}
+                onClick={() => handleDelete(id)}
+              />
+            )
+          })
+      }
     </MenuStyled>
   )
 }
@@ -46,4 +75,23 @@ const MenuStyled = styled.div`
   font-family: "Open Sans", sans-serif;
   overflow: scroll;
   overflow-x: unset;
+
+  .generate-items-btn {
+    font-family: "Open Sans", sans-serif;
+    width: 50%;
+    margin: auto;
+    border-radius: ${theme.borderRadius.round};
+    border: none;
+    padding: 16px 0;
+    font-size: ${theme.fonts.size.P0};
+    background-color: ${theme.colors.primary};
+    color: ${theme.colors.white};
+    }
+  .generate-items-btn:hover {
+    cursor: pointer;
+    background-color: ${theme.colors.white};
+    color: ${theme.colors.primary};
+    outline: 2px solid ${theme.colors.primary};
+    outline-offset: -2px;
+  }
 `;
---
+++ src/components/pages/order/main/MenuEmpty.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components';
+import PrimaryButton from '../../../reusable-ui/PrimaryButton';
+import { theme } from '../../../../theme';
+
+export default function MenuEmpty({text1, text2, btnLabel, btnClassName, onClick}) {
+  return (
+    <MenuEmptySyled>
+        <h1>{text1}</h1>
+        <h2>{text2}</h2>
+            { btnLabel
+                ? <PrimaryButton label={btnLabel} className={btnClassName} onClick={onClick}/>
+                : null
+            }
+    </MenuEmptySyled>
+  )
+}
+const MenuEmptySyled = styled.div`
+    text-align: center;
+    padding: 8px 16px;
+    font-family: "Amatic SC", cursive;
+    font-size: ${theme.fonts.size.P1};
+    width: 100%;
+
+    h1{
+        color: ${theme.colors.greyDark};  
+    }
+    h2{
+        color: ${theme.colors.greySemiDark}; 
+    }
+`;
