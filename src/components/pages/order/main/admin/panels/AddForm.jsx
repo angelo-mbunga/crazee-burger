cc81a1fe181db98e4281479efb615e312b43b204
angelo mbunga <a.mbunga@gmail.com>
2024-05-05 16:45:11 +0200

    ♻️ F09 : PANEL > Add product > Create reusable form

--- src/components/pages/order/main/admin/panels/AddForm.jsx
+++ src/components/pages/order/main/admin/panels/AddForm.jsx
@@ -1,18 +1,16 @@
-import React, { useContext } from 'react'
-import styled from 'styled-components';
+import React, { useContext } from 'react';
 import { FiCheck } from "react-icons/fi";
-import {TextInput} from '../../../../../reusable-ui/TextInput';
-import PrimaryButton from '../../../../../reusable-ui/PrimaryButton';
 import imageByDefault from '../../../../../../assets/img/coming-soon.png';
-import { theme } from '../../../../../../theme';
 import OrderContext from '../../../../../../context/OrderContext';
 import { Zoom, toast } from 'react-toastify';
-import { getTextInputs } from './getTextInputs';
 import { EMPTY_PRODUCT } from '../../../../../../enums/product';
+import ProductForm from './ProductForm';
+import styled from 'styled-components';
 
 export default function AddForm() {
 
-    const {addProductToMenu, newProduct, setNewProduct } = useContext(OrderContext)
+    const {addProductToMenu, newProduct, setNewProduct} = useContext(OrderContext)
+
     let newProductToAdd = {
       id : crypto.randomUUID(),
       imageSource : newProduct.imageSource ? newProduct.imageSource : imageByDefault,
@@ -40,100 +38,20 @@ export default function AddForm() {
             closeOnClick: false,
             pauseOnHover: false,
             draggable: false,
-          }) 
+        }) 
         setNewProduct(EMPTY_PRODUCT)
     }
-    const textInputs = getTextInputs(newProduct);
 
     return (
         <AddFormStyled>
-            <div className='preview'>
-                { newProduct.imageSource 
-                    ? <img src={newProduct.imageSource} alt={newProduct.title} />
-                    : <span className='emptyImageMsg'>Pas d'image</span>
-                }
-            </div>
-            <div className='form'>
-                <form onSubmit={handleSubmit}>
-                    {textInputs.map((input) => (
-                        <TextInput 
-                            key={input.id}
-                            name={input.name}
-                            value={input.value}
-                            onChange={handleChange}
-                            placeholder={input.placeholder}
-                            Icon={input.Icon}
-                        />
-                    ))}
-                    <PrimaryButton
-                        label={"Ajouter le produit"} 
-                        className={"ctaBtn"}
-                    />
-                </form>
-            </div>
+            <ProductForm 
+                onSubmit={handleSubmit} 
+                onChange={handleChange} 
+                product={newProduct}
+                formFooterContent="Ajouter le produit" 
+                formFooterClass="formFooterBtn"
+            />
         </AddFormStyled>
     )
 }
-
-const AddFormStyled = styled.div`
-    display: grid;
-    grid-template-columns: 30% 69%;
-    grid-gap: 8px;
-    padding: 24px;
-
-    .preview{
-        display: flex;
-        justify-content: start;
-        align-items: start;
-
-        .emptyImageMsg{
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: ${theme.fonts.size.P0};
-        }
-        img {
-            border-radius: ${theme.borderRadius.round};
-            width: 66%;
-            height: 80%;
-            margin: auto;
-            object-fit: contain;
-            object-position: center;
-        }
-    }
-    .form{
-        div {
-            display: block;
-            float: left;
-            margin-left: 8px;
-            height: 24px;
-        }
-    }
-    input{
-        padding: 10px 12px;
-        padding-left: 64px;
-        background-color: ${theme.colors.background_white};
-        height: 16px;
-    }
-    .inputIcon {
-        height: 16px;
-        width: 16px;
-        margin-top: -3px;
-    }
-    .input {
-        width: 100%;
-    }
-    .ctaBtn{
-        width: 50%; 
-        padding: 10px;
-        background-color: ${theme.colors.success};
-        color: ${theme.colors.white};
-        border: none;
-        cursor: pointer;
-    }
-    .ctaBtn:hover{
-        background-color: ${theme.colors.green};
-    }
-`;
+const AddFormStyled = styled.div``;
---
+++ src/components/pages/order/main/admin/panels/ProductForm.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import { getTextInputs } from './getTextInputs';
+import { TextInput } from '../../../../../reusable-ui/TextInput';
+import PrimaryButton from '../../../../../reusable-ui/PrimaryButton';
+import styled from 'styled-components';
+import { theme } from '../../../../../../theme';
+
+export default function ProductForm({onSubmit, onChange, product, formFooterContent, formFooterClass}) {
+
+    const textInputs = getTextInputs(product)
+
+    return (
+        <ProductFormStyled>
+            <div className='preview'>
+                { product.imageSource 
+                    ? <img src={product.imageSource} alt={product.title} />
+                    : <span className='emptyImageMsg'>Pas d'image</span>
+                }
+            </div> 
+            <form onSubmit={onSubmit} onChange={onChange}>
+                {textInputs.map((input) => (
+                    <TextInput 
+                        key={input.id}
+                        name={input.name}
+                        value={input.value}
+                        onChange={onChange}
+                        placeholder={input.placeholder}
+                        Icon={input.Icon}
+                    />
+                ))}
+                    <PrimaryButton
+                        label={formFooterContent} 
+                        className={formFooterClass}
+                    />
+            </form>
+        </ProductFormStyled>
+  )
+}
+const ProductFormStyled = styled.form`
+    display: grid;
+    grid-template-columns: 30% 69%;
+    grid-gap: 8px;
+    padding: 24px;
+
+    .preview {
+        display: flex;
+        justify-content: start;
+        align-items: start;
+
+        .emptyImageMsg {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: ${theme.fonts.size.P0};
+        }
+        img {
+            border-radius: ${theme.borderRadius.round};
+            width: 66%;
+            height: 80%;
+            margin: auto;
+            object-fit: contain;
+            object-position: center;
+        }
+    }
+    form {
+        div {
+            display: block;
+            float: left;
+            margin-left: 8px;
+            height: 24px;
+        }
+    }
+    input {
+        padding: 10px 12px;
+        padding-left: 64px;
+        background-color: ${theme.colors.background_white};
+        height: 16px;
+    }
+    .inputIcon {
+        height: 16px;
+        width: 16px;
+        margin-top: -3px;
+    }
+    .input {
+        width: 100%;
+    }
+    .formFooterBtn {
+        width: 50%; 
+        padding: 10px;
+        background-color: ${theme.colors.success};
+        color: ${theme.colors.white};
+        border: none;
+        cursor: pointer;
+    }
+    .formFooterBtn:hover {
+        background-color: ${theme.colors.green};
+    } 
+`
