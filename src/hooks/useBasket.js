4e4d2dc49aafaf55c147eb1ab86bacc6a52c0727
angelo mbunga <a.mbunga@gmail.com>
2024-05-13 21:07:57 +0200

    ♻️ F11 : BASKET > Add > Add product value by default if value is not a number

--- src/hooks/useBasket.js
+++ src/hooks/useBasket.js
@@ -8,19 +8,25 @@ export function useBasket() {
     const addProductToBasket = (productToAdd) => {
         const basketCopy = deepClone(basket)
         const isProductAlreadyInBasket = basketCopy.find((product) => {
-            if (product.title === productToAdd.title) {
+            if (product.id === productToAdd.id) {
                 return true 
             }
         })
 
         if (isProductAlreadyInBasket) {
             // Increment Product in basket
-            const productAlreadyInBasket = basketCopy.find((product) => product.title === productToAdd.title);
+            const productAlreadyInBasket = basketCopy.find((product) => product.id === productToAdd.id);
             productAlreadyInBasket.count++;
             setBasket(basketCopy);
         } 
         else {
-            basketCopy.unshift({id:productToAdd.id, imageSource:productToAdd.imageSource, title:productToAdd.title, price:productToAdd.price, count:1})
+            basketCopy.unshift({
+                id: productToAdd.id,
+                imageSource: productToAdd.imageSource, 
+                title: productToAdd.title, 
+                price: isNaN(productToAdd.price) ? productToAdd.price = "0.00 €" : productToAdd.price,
+                count:1
+            })
             setBasket(basketCopy)
         }
         
@@ -35,6 +41,10 @@ export function useBasket() {
     const calculBasketAmount = (basket) => {
         const basketCopy = deepClone(basket)
         let amount = basketCopy.reduce((previousValue, currentValue) => {
+
+            // Dont add products with wrong price value
+            if (isNaN(currentValue.price)) { return previousValue }
+
             return previousValue + currentValue.count * currentValue.price;
         }, 0);
         setBasketTotalAmount(amount)     
