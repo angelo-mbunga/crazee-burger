28d303d46cde4f15841815715da189ef59db52e2
angelo mbunga <a.mbunga@gmail.com>
2024-07-29 14:03:36 +0200

    🍱 F15 : MENU > Card > Add unavailable img

--- src/enums/product.js
+++ src/enums/product.js
@@ -8,6 +8,7 @@ export const EMPTY_PRODUCT = Object.freeze({
     isAdvertised: false
 })
 export const defaultImage = "/src/assets/img/coming-soon.png";
+export const unavailableImage = "/src/assets/img/stock-epuise.png";
 export const BASKET_MESSAGE = {
     EMPTY : 'Votre panier est vide.',
     LOADING : 'Chargement ...'
